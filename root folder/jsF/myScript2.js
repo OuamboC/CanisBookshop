c81bc9cd9eb4e87339d51eccb833619785b3ee68
Canis Ouambo <118950061+OuamboC@users.noreply.github.com>
2025-01-18 23:07:38 +0000

    Implement addEventListener to ensure feauther icons are initialise

--- root folder/jsF/myScript2.js
+++ root folder/jsF/myScript2.js
@@ -1,4 +1,8 @@
+let a = window.location.href;
+let b = a.replace(/.*=/, "");
+document.getElementById("lastfour").innerHTML = " **** **** **** " + b;
 
-    let a = window.location.href
-    let b = a.replace(/.*=/, '')
-    document.getElementById('lastfour').innerHTML = ' **** **** **** ' + b
+// Ensure Feather icons are initialized
+document.addEventListener("DOMContentLoaded", () => {
+  feather.replace();
+});
